5d9ad5268f6f641981a5247d7af7943ca97f838c
Scott Alton <scottaltonmusic@gmail.com>
2021-04-08 21:13:19 -0400

    Restructured project directories - broke into Server directory.

--- Server/Config/app.ts
+++ Server/Config/app.ts
@@ -7,40 +7,40 @@ import logger from 'morgan';
 import mongoose from 'mongoose';
 
 // App configuration
-import * as indexRouter from './Routes/index';
+import * as indexRouter from '../Routes/index';
 export const app = express();
 
 // DB configuration
-import * as DBConfig from './Config/db';
-mongoose.connect(DBConfig.Path, {useNewUrlParser: true, useUnifiedTopology: true});
+import * as DBConfig from './db';
+mongoose.connect(DBConfig.Path, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   console.log(`Connected to MongoDB at: ${DBConfig.Path}`);
 });
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'Views'));
+app.set('views', path.join(__dirname, '../Views/'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'Client')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../../Client/')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter.router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err:createError.HttpError, req:express.Request, res:express.Response, next:express.NextFunction) {
+app.use(function (err: createError.HttpError, req: express.Request, res: express.Response, next: express.NextFunction) {
   // set locals, only providing error in development
 
   let message = err.message;
@@ -48,7 +48,7 @@ app.use(function(err:createError.HttpError, req:express.Request, res:express.Res
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {message: message, error:error, title: '', page: '', displayName: ''});
+  res.render('error', { message: message, error: error, title: '', page: '', displayName: '' });
 });
 
 //module.exports = app;
