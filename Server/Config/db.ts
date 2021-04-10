814ceab3132c2e756cd00732472933d0c27fbca1
Scott Alton <scottaltonmusic@gmail.com>
2021-04-09 22:04:48 -0400

    Switch database to MongoDB Atlas.

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,7 +1,9 @@
-export const URI = "mongodb://localhost/webd6201";
+export const LocalURI = "mongodb://localhost/webd6201";
+export const RemoteURI = "mongodb+srv://altons:Toronto4@webd6201.u1de4.mongodb.net/webd6201?retryWrites=true&w=majority";
 export const Secret = "someSecret";
-
+export const Host = "MongoDB Atlas";
 /* module.exports = {
     Path: mongoDBPath,
     Secret: sessionSecret
 }  */
+
